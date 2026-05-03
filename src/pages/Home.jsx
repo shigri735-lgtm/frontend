b8503f44e298ac8cb3748672948cf88320cdf564
Muhammad Muslim <shigri735@gmail.com>
2026-05-03 17:15:50 +0500

    Updated project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
       </div>
 
       <a
-        href="https://wa.me/923101988289" 
+        href="https://wa.me/923312686870" 
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-[#25D366] text-white p-4 rounded-full shadow-xl hover:scale-110 transition-transform duration-300 z-50 flex items-center justify-center cursor-pointer"
--- src/pages/miniComponents/Hero.jsx
+++ src/pages/miniComponents/Hero.jsx
@@ -311,9 +311,11 @@ const Hero = () => {
               <span className="absolute left-0 -bottom-1 h-[1px] w-0 bg-blue-500 transition-all duration-300 group-hover:w-full" />
             </a>
           ))}
-          <button className="px-6 py-2.5 border border-white/20 rounded-full hover:bg-blue-600 hover:border-blue-600 hover:text-white transition-all duration-300 font-sans font-medium">
-            Book a Call
-          </button>
+          <a   href="https://wa.me/923312686870" target="_blank" rel="noopener noreferrer">
+            <button className="px-6 py-2.5 border border-white/20 rounded-full hover:bg-blue-600 hover:border-blue-600 hover:text-white transition-all duration-300 font-sans font-medium">
+              Book a Call
+            </button>
+          </a>
         </div>
 
         <button className="md:hidden text-white p-2 rounded-full border border-white/10 bg-white/5 hover:bg-white/10 transition-all duration-300" onClick={() => setIsMenuOpen(!isMenuOpen)}>
